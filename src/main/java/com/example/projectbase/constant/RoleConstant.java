b32c88a7d801f3843e88c0d63007d933753635e4
kienpro-dev <tienkiennropro@gmail.com>
2024-08-07 16:07:04 +0700

    fix car owner

--- src/main/java/com/example/projectbase/constant/RoleConstant.java
+++ src/main/java/com/example/projectbase/constant/RoleConstant.java
@@ -3,4 +3,5 @@ package com.example.projectbase.constant;
 public class RoleConstant {
   public static final String ADMIN = "ROLE_ADMIN";
   public static final String USER = "ROLE_USER";
+  public static final String CAR_OWNER = "ROLE_CAR_OWNER";
 }
