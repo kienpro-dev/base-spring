ef59d05ad012e8c24cfbc85b4ed399944a74e16b
tuanntrieu <trieudangtuan18032003@Gmail.com>
2024-08-08 21:51:24 +0700

    fix error page

--- src/main/java/com/example/projectbase/controller/ErrorController.java
+++ src/main/java/com/example/projectbase/controller/ErrorController.java
@@ -1,12 +1,26 @@
 package com.example.projectbase.controller;
 
+import com.example.projectbase.security.CurrentUser;
+import com.example.projectbase.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/error")
 public class ErrorController  {
+
+    @ModelAttribute("currentUser")
+    public UserPrincipal getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserPrincipal) {
+            return (UserPrincipal) authentication.getPrincipal();
+        }
+        return null;
+    }
 
     @GetMapping(value = "/403")
     public String page403() {
@@ -15,7 +29,6 @@ public class ErrorController  {
 
     @GetMapping(value = "/404")
     public String page404() {
-
         return "auth/error/admin/404";
     }
 }
