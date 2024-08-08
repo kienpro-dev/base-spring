fb2c127d9c4d526dba07531cbe1ec191b8df8f6d
kienpro-dev <tienkiennropro@gmail.com>
2024-08-09 01:20:37 +0700

    fix ui

--- src/main/java/com/example/projectbase/controller/HomeController.java
+++ src/main/java/com/example/projectbase/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.example.projectbase.controller;
 
 import com.example.projectbase.base.CarMvc;
 import com.example.projectbase.constant.UrlConstant;
+import com.example.projectbase.domain.entity.Car;
 import com.example.projectbase.domain.entity.User;
 import com.example.projectbase.security.CurrentUser;
 import com.example.projectbase.security.UserPrincipal;
 import com.example.projectbase.service.AuthService;
+import com.example.projectbase.service.CarService;
 import com.example.projectbase.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @CarMvc
 @RequiredArgsConstructor
@@ -22,6 +26,8 @@ public class HomeController {
 
     private final AuthService authService;
 
+    private final CarService carService;
+
     @GetMapping(UrlConstant.HOME)
     public String home(Model model,
                        @CurrentUser UserPrincipal userPrincipal) {
@@ -30,6 +36,12 @@ public class HomeController {
             model.addAttribute("user", currentUser);
             model.addAttribute("role", currentUser.getRole().getName());
         }
+
+        List<Car> listCarPetrol = carService.findAllByFuelType("Petrol");
+        model.addAttribute("listPetrol", listCarPetrol);
+
+        List<Car> listCarElectric = carService.findAllByFuelType("Electric");
+        model.addAttribute("listElectric", listCarElectric);
 
         return "client/home/index";
     }
--- src/main/java/com/example/projectbase/repository/CarRepository.java
+++ src/main/java/com/example/projectbase/repository/CarRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car,String> {
     Page<Car> findByUserOwn(Pageable pageable, User userOwn);
+    List<Car> findByFuelType(String fuelType);
 }
