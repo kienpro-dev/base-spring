81479223ef168d3eaa7b3450d7eca26aea78c564
Cuong day <cuonga22k3@gmail.com>
2024-08-08 23:47:09 +0700

    "fix: update car(image)"

--- src/main/java/com/example/projectbase/controller/HomeController.java
+++ src/main/java/com/example/projectbase/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.example.projectbase.constant.UrlConstant;
 import com.example.projectbase.domain.entity.User;
 import com.example.projectbase.security.CurrentUser;
 import com.example.projectbase.security.UserPrincipal;
+import com.example.projectbase.service.AuthService;
 import com.example.projectbase.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +19,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     private final UserService userService;
+    private final AuthService authService;
 
     @GetMapping(UrlConstant.HOME)
     public String home(Model model,
                        @CurrentUser UserPrincipal userPrincipal) {
-        User currentUser = this.userService.findById(userPrincipal.getId());
-        model.addAttribute("user", currentUser);
-        model.addAttribute("role", currentUser.getRole().getName());
+        if(authService.isAuthenticated()) {
+            User currentUser = this.userService.findById(userPrincipal.getId());
+            model.addAttribute("user", currentUser);
+            model.addAttribute("role", currentUser.getRole().getName());
+        }
         return "client/home/index";
     }
 
