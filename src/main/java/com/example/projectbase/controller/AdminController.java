b8d958eea567865144b81547eaea18d147a1ad18
tuanntrieu <trieudangtuan18032003@Gmail.com>
2024-08-14 00:04:29 +0700

    statistical users,bookings

--- src/main/java/com/example/projectbase/controller/AdminController.java
+++ src/main/java/com/example/projectbase/controller/AdminController.java
@@ -10,8 +10,10 @@ import com.example.projectbase.repository.CarRepository;
 import com.example.projectbase.repository.UserRepository;
 import com.example.projectbase.security.UserPrincipal;
 import com.example.projectbase.service.AuthService;
+import com.example.projectbase.service.BookingService;
 import com.example.projectbase.service.SessionService;
 import com.example.projectbase.service.UserService;
+import com.example.projectbase.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -31,6 +33,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -51,6 +55,11 @@ public class AdminController {
     private final UserRepository userRepository;
 
     private final CarRepository carRepository;
+
+    private final BookingService bookingService;
+
+    private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    private Date datenow = new Date();
 
     @GetMapping(UrlConstant.Auth.ADMIN_LOGIN)
     public String getLoginForm(Model model) {
@@ -99,7 +108,7 @@ public class AdminController {
             return "auth/admin/login";
         }
         authService.autoLogin(loginRequestDto.getEmail(), loginRequestDto.getPassword());
-        return "redirect:/admin-page/home";
+        return "redirect:/admin/home";
     }
 
     @GetMapping(value = UrlConstant.Admin.USERS_MANAGEMENT)
@@ -187,5 +196,86 @@ public class AdminController {
         model.addAttribute("resultPage", resultPage);
         return "admin/products/product-list";
     }
+
+    @GetMapping(value = "/admin/count-order-date-now")
+    public ResponseEntity<Integer> getCountOrderDateNow() {
+        Date dateTo = DateUtil.sumNumberDate(datenow, 1);
+        int countDateNow = bookingService.getBookingsByDate(formatter.format(datenow), formatter.format(dateTo));
+        return new ResponseEntity<Integer>(countDateNow, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/count-order-date-ye")
+    public ResponseEntity<Integer> getCountOrderDateYe() {
+        Date dateYe = DateUtil.sumNumberDate(datenow, -1);
+        int countDateYe = bookingService.getBookingsByDate(formatter.format(dateYe), formatter.format(datenow));
+        return new ResponseEntity<Integer>(countDateYe, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/count-order-month-now")
+    public ResponseEntity<Integer> getCountOrderMonthNow() {
+        int countMonthNow = bookingService.getBookingsByMonth(DateUtil.getMonthNow() + 1);
+        return new ResponseEntity<Integer>(countMonthNow, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/count-order-month-ye")
+    public ResponseEntity<Integer> getCountOrderMonthYe() {
+        int countMonthYe = bookingService.getBookingsByMonth(DateUtil.getMonthNow());
+        return new ResponseEntity<Integer>(countMonthYe, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/count-order-year-now")
+    public ResponseEntity<Integer> getCountOrderYearNow() {
+        int countYearNow = bookingService.getBookingsByYear(DateUtil.getYearNow());
+        return new ResponseEntity<Integer>(countYearNow, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/count-order-year-ye")
+    public ResponseEntity<Integer> getCountOrderYearYe() {
+        int countYearYe = bookingService.getBookingsByYear(DateUtil.getYearNow() - 1);
+        return new ResponseEntity<Integer>(countYearYe, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/admin/users-date-now")
+    public ResponseEntity<Integer> getCustomersDateNow() {
+        Date dateTo = DateUtil.sumNumberDate(datenow, 1);
+        int customersDateNow = userService.getCustomersByDate(formatter.format(datenow), formatter.format(dateTo));
+        return new ResponseEntity<Integer>(customersDateNow, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/users-date-ye")
+    public ResponseEntity<Integer> getCustomersDateYe() {
+        Date dateYe = DateUtil.sumNumberDate(datenow, -1);
+        int customersDateYe = userService.getCustomersByDate(formatter.format(dateYe), formatter.format(datenow));
+        return new ResponseEntity<Integer>(customersDateYe, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/users-month-now")
+    public ResponseEntity<Integer> getCustomersMonthNow() {
+        int customersMonthNow = userService.getCustomersByMonth(DateUtil.getMonthNow() + 1);
+        return new ResponseEntity<Integer>(customersMonthNow, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/users-month-ye")
+    public ResponseEntity<Integer> getCustomersMonthYe() {
+        int customersMonthYe = userService.getCustomersByMonth(DateUtil.getMonthNow());
+        return new ResponseEntity<Integer>(customersMonthYe, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/users-year-now")
+    public ResponseEntity<Integer> getCustomersYearNow() {
+        int customersYearNow = userService.getCustomersByYear(DateUtil.getYearNow());
+        return new ResponseEntity<Integer>(customersYearNow, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/admin/users-year-ye")
+    public ResponseEntity<Integer> getCustomersYearYe() {
+        int customersYearYe = userService.getCustomersByYear(DateUtil.getYearNow() - 1);
+        return new ResponseEntity<Integer>(customersYearYe, HttpStatus.OK);
+    }
+
+
+
+
 
 }
